31b510d601f27c99f0086f04adc6a53c62a1f723
Jules LEIDELINGER <11395311+julio75012@users.noreply.github.com>
2026-03-09 17:29:34 +0800

    formatting

--- DeepSeek-OCR-master/DeepSeek-OCR-hf/run_dpsk_ocr.py
+++ DeepSeek-OCR-master/DeepSeek-OCR-hf/run_dpsk_ocr.py
@@ -3,31 +3,29 @@ import torch
 import os
 
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-#disable for amd
+# os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+# disable for amd
 
-model_name = 'deepseek-ai/deepseek-ocr'
+model_name = "deepseek-ai/deepseek-ocr"
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModel.from_pretrained(
-    model_name, 
-    _attn_implementation='eager',  # Changed from 'flash_attention_2'
-    trust_remote_code=True, 
-    use_safetensors=True
+    model_name,
+    _attn_implementation="eager",  # Changed from 'flash_attention_2'
+    trust_remote_code=True,
+    use_safetensors=True,
 )
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.eval().to(device)
 if torch.cuda.is_available():
     model = model.to(torch.bfloat16)
 
 
-
 # prompt = "<image>\nFree OCR. "
-prompt = "<image>\n<|grounding|>describe the picture "
-image_file = '/home/jules/Pictures/photo_2024-03-19_18-43-16.jpg'
-output_path = './'
-
+prompt = "<image>\n<|grounding|>put a frame around each of the building that you can see in the picture"
+image_file = "/home/jules/Pictures/tai-mo-shan-fun-fact-1920x1080.jpg"
+output_path = "./"
 
 
 # infer(self, tokenizer, prompt='', image_file='', output_path = ' ', base_size = 1024, image_size = 640, crop_mode = True, test_compress = False, save_results = False):
@@ -39,4 +37,14 @@ output_path = './'
 
 # Gundam: base_size = 1024, image_size = 640, crop_mode = True
 
-res = model.infer(tokenizer, prompt=prompt, image_file=image_file, output_path = output_path, base_size = 1024, image_size = 640, crop_mode=True, save_results = True, test_compress = True)
+res = model.infer(
+    tokenizer,
+    prompt=prompt,
+    image_file=image_file,
+    output_path=output_path,
+    base_size=1024,
+    image_size=640,
+    crop_mode=True,
+    save_results=True,
+    test_compress=True,
+)
